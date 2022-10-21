4ef4c21b8c8a818a25d104b9658503324b3eea8e
Bobby Westberg <bellfalasch@gmail.com>
2022-10-21 11:38:36 +0200

    fix: wrap in provider-comp, fix query

--- pages/movies/[movie].tsx
+++ pages/movies/[movie].tsx
@@ -1,23 +1,24 @@
 import { useRouter } from 'next/router'
-import { ApolloClient, InMemoryCache, gql, useQuery } from '@apollo/client';
+import client from "../../apollo-client";
+import { ApolloProvider, gql, useQuery } from '@apollo/client';
 
 const Movie = () => {
   const router = useRouter()
   const { movie } = router.query
 
-  if (movie) {
-    const movieData = MovieInformation(movie as string);
-    console.log(JSON.stringify(movieData,null,4));
-  }
-  return <p>Movie: {movie}</p>
+  const movieData = MovieInformation(movie as string);
+  console.log(JSON.stringify(movieData,null,4))
+  return (
+  <ApolloProvider client={client}>
+    <p>Movie: {movieData?.film?.title}</p>
+  </ApolloProvider>)
 }
 
 export default Movie
 
-
 const GET_MOVIE_DATA = gql`
-  query getFilms {
-    film(id: $movieId) {
+  query getFilm($id: ID!) {
+    film(id: $id) {
       id
       title
       director
@@ -27,41 +28,13 @@ const GET_MOVIE_DATA = gql`
 `;
 
 function MovieInformation(movieId: string) {
-  const client = new ApolloClient({
-    uri: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
-    cache: new InMemoryCache()
-  });
   const { loading, error, data } = useQuery(GET_MOVIE_DATA, {
-    variables: { movieId }
+    client,
+    variables: { id: movieId },
   });
 
   if (loading) return null;
   if (error) return `Error! ${error}`;
 
   return data;
-}
-
-export async function getServerSideProps() {
-  const client = new ApolloClient({
-    uri: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
-    cache: new InMemoryCache()
-  });
-  const { data } = await client.query({
-    query: gql`
-    query getMovies {
-      film(id: "ZmlsbXM6NQ==") {
-        id
-        title
-        director
-        releaseDate
-      }
-    }
-    `
-  });
-  //console.log(data)
-  return {
-    props: {
-      data
-    }
-  }
 }
