46c1699d6e138ba5740270fe46e1ec58bfb3bc58
benedic2s <benedictus.duweh@gjensidige.no>
2022-10-31 23:22:14 +0100

    fix: search

--- pages/search.tsx
+++ pages/search.tsx
@@ -2,11 +2,10 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
-import { useQuery } from '@apollo/client';
-import { useRef } from "react";
+import { useState } from "react";
+import { Card, Col, Row, Input, Space } from 'antd';
 
 import styles from '../styles/Home.module.css'
-
 
 export async function getStaticProps() {
   const client = new ApolloClient({
@@ -15,26 +14,23 @@ export async function getStaticProps() {
   });
   const { data } = await client.query({
       query: gql`
-  query ($searchQuery: String) {
-    allFilms {
-      films(filter:) {
-        id
-        title
-        director
-        releaseDate
-        speciesConnection {
-          species {
-            name
-            classification
-            homeworld {
-              name
+        query {
+          allFilms {
+            films {
+              id
+              title
+              director
+              producers
+              speciesConnection {
+                species {
+                  name
+                  language
+                  averageHeight
+                }
+              }
             }
           }
-        }
-      }
-    }
-  }
-  `
+        }`
   });
   return {
       props: {
@@ -45,45 +41,56 @@ export async function getStaticProps() {
 
 const Search: NextPage = (movies) => {
     const { data } = movies;
-    const clickPoint = useRef();
-    const handleFocus = () => {
-        clickPoint.current.style.display = "none";
+    const [search, setNewSearch] = useState("");
+    const [searchDB, setSearchDB] = useState(data?.allFilms.films);
+
+    const { Search } = Input;
+    const handleSearchChange = (e) => {
+      setNewSearch(e.target.value);
     };
 
-    const handleBlur = () => {
-        clickPoint.current.style.display = "block";
-    };
+    const filtered = !search
+    ? searchDB
+    : searchDB.filter((film) =>
+        film.title.toLowerCase().includes(search.toLowerCase())
+      );
   return (
-    <div className={styles.container}>
+    <div className="container my-12 mx-auto px-4 md:px-12">
       <Head>
-        <title>Search Movies Database</title>
+        <title className="text-3xl text-green-600 p-2">Search Movies Database</title>
         <meta name="description" content="Search Star wars movie Data base" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-        Search Star wars movies Database
+      <main className="">
+        <h1 className="">
+          Search Star wars movies Database
         </h1>
 
-        <div className="items-center px-4 flex justify-center" >
-            <div className="relative mr-3">
-                <div className="absolute top-3 left-3 items-center" ref={clickPoint}>
-                    <svg className="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-                </div>
-                <input
-                    type="text"
-                    className="block p-2 pl-10 w-70 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:pl-3"
-                    placeholder="Search Here..."
-                    onFocus={handleFocus}
-                    onBlur={handleBlur}
-                />
-            </div>
-        </div>
+        <Space direction="vertical">
+          <Search
+            placeholder="input search text"
+            allowClear
+            enterButton="Search"
+            size="large"
+            onChange={handleSearchChange}
+          />
+        </Space>
+        
 
-        {data.allFilms.films.map((film) => (
-          <div key={film.id}>{film.title}</div>
-        ))}
+        <div className="site-card-wrapper">
+            <Row gutter={16}>
+              {filtered.map((film) => (
+                <Col span={8}>
+                  <a href={`/movies/${film.id}`} key={film.id}>
+                    <Card key={film.id} title={film.title} bordered={false}>
+                      Director: {film.director}
+                    </Card>
+                  </a>
+                </Col>
+              ))}
+            </Row>
+          </div>
       </main>
 
       <footer className={styles.footer}>
