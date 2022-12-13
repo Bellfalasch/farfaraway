ced2cafbbd3479dc8bb1a849a5ed533723cf42a0
Bente Midtsveen <bente.midtsveen@gjensidige.no>
2022-12-13 15:20:55 +0100

    fix: add picture

--- pages/actors/index.tsx
+++ pages/actors/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import { Typography, Pagination } from 'antd';
+import Image from 'next/image';
+import starWars from '../../public/darth-vader.jpg'
 
 export async function getServerSideProps(context) {
     console.log(context.query)
@@ -39,6 +41,12 @@ const Actors: NextPage = (actor) => {
         <>
     <Typography>
       <Title>CHARACTERS</Title>
+      <Image
+        src={starWars}
+        alt="Darth Vader"
+        width={350}
+        height={200}
+      />
       <Paragraph>An individual person or character within the Star Wars universe.</Paragraph>
       <ul>
             {data.map((char) => (
