8d24953c5e1af3a8bc6513618872ad3fb343933a
Bobby Westberg <bellfalasch@gmail.com>
2022-12-22 10:28:23 +0100

    fix: forgot one important await ...

--- app/movies/[movies]/page.tsx
+++ app/movies/[movies]/page.tsx
@@ -1,15 +1,15 @@
 
-export default function Page({
+async function Page({
   params,
 }: {
   params: { movies: string };
 }) {
   console.log(params.movies);
-  const movieData = getMovieData(params.movies as string);
+  const movieData = await getMovieData(params.movies as string);
   if (movieData) {
     console.log(JSON.stringify(movieData,null,4));
     // @ts-ignore
-    return <h1>Hello {movieData?.film?.title}</h1>;
+    return <h1>Hello {movieData?.data?.film?.title}</h1>;
   } else {
     return <h1>Nothing found</h1>
   }
@@ -42,18 +42,26 @@ async function getMovieData(id: string) {
   `;
   //console.log(JSON.stringify(query,null,4));
 
-  const response = await fetch('https://swapi-graphql.netlify.app/.netlify/functions/index', {
-    method: 'POST',
-    headers: {
-      "content-type": "application/json",
-      "Cache-Control": "no-store",
-    },
-    body:
-      JSON.stringify({
-        "query": query,
-        "variables": { "id": cleaned }
-      }),
-  })
+  const response = await fetch(
+    'https://swapi-graphql.netlify.app/.netlify/functions/index',
+    {
+      method: 'POST',
+      headers: {
+        "content-type": "application/json",
+        "Cache-Control": "no-store",
+      },
+      body:
+        JSON.stringify({
+          query: query,
+          variables: { id: cleaned }
+        }),
+    }
+  );
+  //.then(response => { const res = response.json(); console.log(res) })
+  //.then(data => { console.log('data',data); return data });
+  //console.log(response.ok);
+  //console.log(response.status);
   return await response.json();
-
 }
+
+export default Page;
